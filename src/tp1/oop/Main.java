78e43055d864e4aefed4b5e8be5bc1facb0465ab
Harrist Muhammad <harristfhs@gmail.com>
2023-09-06 22:54:27 +0700

    First Commit

---
+++ src/tp1/oop/Main.java
@@ -0,0 +1,10 @@
+package tp1.oop;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println("Welcome to my new Java project!git");
+    }
+
+}
